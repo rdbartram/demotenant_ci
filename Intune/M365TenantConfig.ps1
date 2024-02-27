73aa9b78661f16ebfdf7a7cffbab10dfb5801af0
Intune CI <username@users.noreply.github.com>
2024-02-27 08:32:27 +0000

    Intune Configuration Export

---
+++ Intune/M365TenantConfig.ps1
@@ -0,0 +1,63 @@
+# Generated with Microsoft365DSC version 1.24.221.1
+# For additional information on how to use Microsoft365DSC, please visit https://aka.ms/M365DSC
+param (
+    [parameter()]
+    [System.Management.Automation.PSCredential]
+    $Credential
+)
+
+Configuration M365TenantConfig
+{
+    param (
+        [parameter()]
+        [System.Management.Automation.PSCredential]
+        $Credential
+    )
+
+    if ($null -eq $Credential)
+    {
+        <# Credentials #>
+        $Credscredential = Get-Credential -Message "Credentials"
+
+    }
+    else
+    {
+        $CredsCredential = $Credential
+    }
+
+    $OrganizationName = $CredsCredential.UserName.Split('@')[1]
+
+    Import-DscResource -ModuleName 'Microsoft365DSC' -ModuleVersion '1.24.221.1'
+
+    Node localhost
+    {
+        IntuneSettingCatalogCustomPolicyWindows10 "IntuneSettingCatalogCustomPolicyWindows10-87d92e51-d36e-418f-9c97-81f3c3f64965"
+        {
+            Assignments          = @();
+            Credential           = $Credscredential;
+            Description          = "";
+            Ensure               = "Present";
+            Id                   = "87d92e51-d36e-418f-9c97-81f3c3f64965";
+            Name                 = "My Settings";
+            Platforms            = "windows10";
+            Settings             = @(
+                MSFT_MicrosoftGraphdeviceManagementConfigurationSetting{
+
+                    SettingInstance = MSFT_MicrosoftGraphDeviceManagementConfigurationSettingInstance{
+
+                        choiceSettingValue = MSFT_MicrosoftGraphDeviceManagementConfigurationChoiceSettingValue{
+
+                            Value = 'device_vendor_msft_policy_config_authentication_allowaadpasswordreset_1'
+                        }
+                        odataType = '#microsoft.graph.deviceManagementConfigurationChoiceSettingInstance'
+                        SettingDefinitionId = 'device_vendor_msft_policy_config_authentication_allowaadpasswordreset'
+                    }
+
+                }
+            );
+            Technologies         = "mdm";
+        }
+    }
+}
+
+M365TenantConfig -ConfigurationData .\ConfigurationData.psd1 -Credential $Credential
